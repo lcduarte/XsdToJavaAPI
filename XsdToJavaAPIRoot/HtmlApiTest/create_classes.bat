1e5e2f1503e9a2f53224ac0e8a2430b9dc5d8f3b
lcduarte <lois_duarte@msn.com>
2018-04-11 18:15:55 +0100

    * Removes unnecessary dependency between HtmlApiTest and HtmlApi;

--- XsdToJavaAPIRoot/HtmlApiTest/create_classes.bat
+++
@@ -1,4 +0,0 @@
-if exist "./src/main/java" rmdir "./src/main/java" /s /q
-mkdir "./src/main/java/org/xmlet/htmlapi"
-call mvn exec:java -D"exec.mainClass"="org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler" -D"exec.args"="-dgs=true ./../HtmlApi/target/classes/org/xmlet/htmlapi ./src/main/java/org/xmlet/htmlapi"
-ECHO ON
