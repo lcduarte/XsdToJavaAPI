324987d16c0c69082f0aba016f9f8cb1cfba4c89
lcduarte <lois_duarte@msn.com>
2018-06-21 17:23:46 +0100

    * HtmlApi 1.0.9 release;

--- XsdToJavaAPIRoot/XsdAsm/src/test/java/org/xmlet/xsdasm/XsdAsmBasicTest.java
+++ XsdToJavaAPIRoot/XsdAsm/src/test/java/org/xmlet/xsdasm/XsdAsmBasicTest.java
@@ -3,6 +3,7 @@ package org.xmlet.xsdasm;
 import org.junit.Test;
 import org.xmlet.xsdasm.classes.XsdAsm;
 import org.xmlet.xsdasm.classes.XsdAsmUtils;
+import org.xmlet.xsdasm.main.XsdAsmMain;
 import org.xmlet.xsdparser.core.XsdParser;
 
 import java.io.File;
@@ -14,9 +15,12 @@ public class XsdAsmBasicTest {
     private static final String HTML_FILE_NAME = XsdAsmBasicTest.class.getClassLoader().getResource("html_5.xsd").getPath();
     private static final String HTML_API_NAME = "Html5";
 
+    static {
+        XsdAsmMain.main(new String[]{HTML_FILE_NAME, HTML_API_NAME});
+    }
+
     @Test
     public void testGeneratedClassesIntegrity() throws Exception {
-        new XsdAsm().generateClassFromElements(new XsdParser().parse(HTML_FILE_NAME), HTML_API_NAME);
         testIntegrity(HTML_API_NAME);
     }
 
